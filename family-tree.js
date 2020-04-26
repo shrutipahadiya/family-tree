a6ba9e8e6f96f80c3824e56daa50b9c8950349a6
Shruti Pahadiya <15.shruti@gmail.com>
2020-04-26 17:35:12 -0400

    sol

--- family-tree.js
+++ family-tree.js
@@ -24,8 +24,26 @@ class FamilyTree {
     return this.children.pop(value);
   }
 
-  log(){
-    if (this.children != null) {
+  log(answer,this){
+      answer = "--";
+      if(this.children.length == 0){
+          ans = ans+this.value;
+         // console.log("111 == "+ans);
+      }
+      
+      if(this.children.length > 0){
+      //else{
+        for(let i =0;i<this.children.length;i++){
+          ans = ans+"-- "+(this.children[i]).value;
+          ans=ans+"-- "+log(answer,this.children[i]);
+         // console.log("222 == "+ans);
+        }
+      }
+     // console.log("final == "+ans);
+      return ans;
+
+
+   /* if (this.children != null) {
       for (var x = 0; x <= this.children.length; x++) {
         if (this.children[x] != null) {
           for (var y = 0; y <= this.children[y].length; y++) {
@@ -37,7 +55,7 @@ class FamilyTree {
       }
     } else {
       console.log("-- " + this.value);
-    }
+    }*/
   }
 }
 
